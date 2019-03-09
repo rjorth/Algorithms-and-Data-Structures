fd1255c35b1976074f304e91753d6efd4bc4064d
Rose Orth <rosaliejorth@gmail.com>
2019-03-09 11:39:17 -0500

    faster solution

--- twoSum.py
+++ twoSum.py
@@ -1,11 +1,4 @@
- """"
- Leetcode:
- Given an array of integers, return indices of the 
- two numbers such that they add up to a specific target.
 
-You may assume that each input would have exactly one solution, 
-and you may not use the same element twice.
- """
 
 def twoSum(nums, target):
         """
@@ -13,12 +6,11 @@ def twoSum(nums, target):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target:
-                    if i != j:
-                        return i, j
-                        print(i, j)
+        d = {}
+        for i, num in enumerate(nums):
+            if target - num in d:
+                return d[target-num], i
+            d[num] = i 
 
 
 print(twoSum([1,2,3,4], 5))
