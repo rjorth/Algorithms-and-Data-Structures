36a7bb44de19014ad7dd05d58afecf339a3c76c4
Rose Orth <rosaliejorth@gmail.com>
2019-03-11 16:11:29 -0400

    better code for removeElement

--- removeElement.py
+++ removeElement.py
@@ -3,10 +3,10 @@ def removeElement(nums, val):
     n = len(nums)
     while (i < n):
         if nums[i] == val:
-            nums[i] = nums[i-n]
+            nums[i] = nums[n-1]
             n -= 1
         else:
             i += 1
-    return nums 
+    return n 
 
 print(removeElement([1,2,3,4,4,4,5,5,6], 1))
