fb6e6c55e1024be091457b7f328bcc316fd4df4d
Rose Orth <rosaliejorth@gmail.com>
2019-03-12 22:58:53 -0400

    solution to happy number

--- happyNumber.py
+++ happyNumber.py
@@ -1,1 +1,18 @@
 def isHappy(n):
+	
+	#get unique values, prevents loop from going infinitely
+	seen = set()
+	while n != 1:
+		n = sum([int(i) ** 2 for i in str(n)])
+		if n in seen:
+			return False 
+		else:
+			seen.add(n)
+	else:
+		return True
+	
+	
+
+
+print(isHappy(19))
+print(isHappy(0))
