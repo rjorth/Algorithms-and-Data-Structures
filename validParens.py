b45f48d8b7078f361350e0e43d1f4124a1a04fb5
Rose Orth <rosaliejorth@gmail.com>
2019-03-13 22:48:06 -0400

    solution to parentheses with comments

--- validParens.py
+++ validParens.py
@@ -1,11 +1,18 @@
 def validp(s):
-	stack = [] 
+	stack = []  #store iterated vals
+	#dict is obvious
 	d = {
 	"]":"[",
 	"}":"{",
 	")":"("
 
 	}
+	#iterate through input
+	#if a left holding bracker is crossed, then push 
+	#if a right holding bracket is crossed, check that stack is empty
+	#if stack is empty, then false bc no left holding
+	#otherwise, most recent val must be corresponding left holding
+	#return if stack empty, true, if stack contains, false 
 
 	for element in s:
 		if element in d.values():
