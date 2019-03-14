2544ef55af971e610389670aa723bfc6a115cf0c
Rose Orth <rosaliejorth@gmail.com>
2019-03-14 00:05:36 -0400

    solution to roman with comments tested

--- romans3.py
+++ romans3.py
@@ -9,16 +9,17 @@ def toRoman(s):
 	"I": 1 
 	}
 	#get dict, obvious
-	result = 0 
+	pre = 0 
+	post = 0
 	#set ret, hold return val 
 
-	for i in range(0,len(s)):
+	for i in range(0,len(s)-1): #avoid indexing error
 		#loop 0 to length
 		#if num in dict less than following num in dict, its to subtract
-		if roman[s[i]] < roman[s[i+1]]:
+		if (i < len(s) -1) and roman[s[i]] < roman[s[i+1]]:
 			result -= roman[s[i]]
 		else:
 			#if num in dict greater than following, then to add 
 			result += roman[s[i]]
 	#get result, plus final addition which is always an add 
-	return result += roman[s[-1]]
+	return result + roman[s[-1]]
