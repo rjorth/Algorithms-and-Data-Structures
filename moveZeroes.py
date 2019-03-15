49e404a7d2aa81b2d914ba482e55e8dcbcb96e14
Rose Orth <rosaliejorth@gmail.com>
2019-03-15 13:10:48 -0400

    edits on move zeroes

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,5 +1,10 @@
 def moveZeroes(nums):
+	#iterate backwards! 
 	for i in range(len(nums))[::-1]:
 		if nums[i] == 0:
+			#pop where i is zero. does this count as in place?  
 			nums.pop(i)
 			nums.append(0)
+	return nums
+
+print(moveZeroes([1,2,1,0,0,3]))
