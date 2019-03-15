e40c0cf7d648aa94559be5d8c7f83c551af54791
Rose Orth <rosaliejorth@gmail.com>
2019-03-15 12:33:17 -0400

    max depth of bin tree, but i think i just have it memorized

---
+++ maxDepthofBinTree.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+def maxDepth(root): 
+	if not root:
+		return 0 
+
+	return 1 + max(maxDepth(root.left), maxDepth(root.right)
