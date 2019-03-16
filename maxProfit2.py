23ddc4742f5a2cc04a240c5f2a38092010886d3f
Rose Orth <rosaliejorth@gmail.com>
2019-03-15 20:40:34 -0400

    not a faster solution

--- maxProfit2.py
+++ maxProfit2.py
@@ -5,3 +5,14 @@ def profit(prices):
 		if prices[i] > prices[i-1]:
 			profit += prices[i] - proces[i-1]
 	return profit 
+
+def oneLine(prices):
+	arr = [] 
+	for i in range(len(prices)-1):
+		if prices[i+1] > prices[i]:
+			profit = prices[i+1] - prices[i] 
+			arr.append(profit)
+	return sum(arr)
+
+
+print(oneLine([7,1,5,3,6,4]))
