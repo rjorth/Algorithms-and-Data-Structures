0e5259501009817a776d078a66d4e75392b86b94
Rose Orth <rosaliejorth@gmail.com>
2019-03-18 20:55:10 -0400

    instersection of arrays solution

---
+++ intersectofarrays.py
@@ -0,0 +1,20 @@
+def intersect(nums1, nums2):
+	nums1, nums2, = sorted(nums1), sorted(nums2)
+
+	p = pt = 0 
+
+	res = [] 
+
+	while True:
+		try:
+			if nums2[p] > nums2[pt]:
+				pt ++ 1 
+			elif nums1[p] < nums2[pt]:
+				p += 1 
+			else:
+				res.append(nums1[pt])
+				p += 1
+				pt += 1
+		except IndexError:
+			break 
+	return res
