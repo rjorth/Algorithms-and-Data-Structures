f3f4a1f98503351900583024b59c85d745ede764
Rose Orth <rosaliejorth@gmail.com>
2019-03-18 22:29:35 -0400

    edits

--- twoSumSOlution.py
+++ twoSumSOlution.py
@@ -5,6 +5,8 @@ def twoSum(nums, target):
         if target - val in d: 
             return d[target-val], index
             
-    	d[val] = index 
+    	d[val] = index
+    	#only passes all cases when this line is here, and not
+    	#first in the loop. 
 
 print(twoSum([2,7,11,15], 9))
