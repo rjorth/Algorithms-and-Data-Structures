d496f6f3cb838c321672ccd12977b820c579073c
Rose Orth <rosaliejorth@gmail.com>
2019-03-18 23:10:16 -0400

    solution to mirror tree

---
+++ isSymmetric.py
@@ -0,0 +1,12 @@
+def isSymmetric(root):
+	if root:
+		return self.helper(root.left, right.right)
+	else:
+		return True
+
+def helper(p, q):
+	if p and q:
+		return p.val == q.val and
+		helper(p.left, q.right) and helper(p.right, q.left)
+	else:
+		return not p and not q 
