a554ea34cf1ab6b34837424f28d386d32a34cf4c
Rose Orth <rosaliejorth@gmail.com>
2019-03-18 23:54:08 -0400

    works for me, but not for leetcode

---
+++ validPalindromeStr.py
@@ -0,0 +1,11 @@
+def valid(s):
+	s = s.lower()
+	s = s.strip().split(' ')
+	s = "".join(char for char in s if char.isalnum())
+ 
+
+	return s == s[::-1]
+
+print(valid("Race car"))
+print(valid("fat"))
+print(valid(":racecar"))
