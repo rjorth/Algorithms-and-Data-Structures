b9f9e43827be0daa9b2c83a1eec27b6a4210bc97
Rose Orth <rosaliejorth@gmail.com>
2019-03-18 21:22:42 -0400

    edits to power of three

--- powerOfThree.py
+++ powerOfThree.py
@@ -1,6 +1,8 @@
 def powerOfThree(n):
 
 #if it's a power, then it should be divisble by zero until n is 1
+	if n < 1:
+		return False #or else time limit exceeded 
 	while (n % 3 == 0): 
 
 		n /= 3 
